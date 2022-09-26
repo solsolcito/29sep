a507a9849c3d5a278294a4b32a168992d4659583
solange alarcon <solealarconmelinao@gmail.com>
2022-09-26 11:14:38 -0300

    clase 26 de sep

---
+++ main.js
@@ -0,0 +1,27 @@
+let number = 1
+do {
+    console.log(number)
+    number = number + 1 // es igual a n ++
+
+} while(number<=10)
+
+let number2 = 10
+do{
+    console.log(number2)
+    number2 = number2 - 1 // es igual a n --
+ 
+} while( number2>=1)
+
+const name="solange"
+let n= name.length-1
+do{ 
+    console.log(name[n])
+    n=n-1
+}while(n>=0)
+
+const lastname="alarcon"
+let n1=0
+do{
+    console.log(lastname[n1])
+    n1=n1+1
+}while(n1 <=lastname.length-1)
